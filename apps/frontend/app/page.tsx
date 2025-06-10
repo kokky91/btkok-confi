0f9dfa1c4f8f436767d842ab19e98e684e10b578
kokky91 <diederikkok123@hotmail.com>
2025-06-10 02:01:21 +0200

    Delete apps/frontend directory

--- apps/frontend/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomePage() {
-  return <h2>Welkom bij de frontend-app!</h2>;
-}
--- apps/frontend/src/hooks/useUser.ts
+++
@@ -1,5 +0,0 @@
-import { useState } from 'react';
-export function useUser() {
-  const [user, setUser] = useState(null);
-  return { user, setUser };
-}
